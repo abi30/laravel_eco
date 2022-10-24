1f91d7c95c86957aa77fb21afc1a80a87acb7535
abi30 <rakib0751@gmail.com>
2022-10-24 16:48:42 +0200

    going to push -u

---
+++ app/Http/Middleware/Roles.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+class Roles
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $routeName = Route::getFacadeRoot()->current()->uri();
+        $route = explode('/', $routeName);
+
+        if (!auth()->check()) {
+            dd("hello1");
+            if ($route[0] == 'admin') {
+                return redirect()->route('admin.login');
+            }
+            return redirect()->route('login');
+        }
+
+        if (auth()->user()->isAdminOrSupervisor()) {
+            dd("hello2");
+            return $next($request);
+        }
+
+        return back();
+    }
+}
